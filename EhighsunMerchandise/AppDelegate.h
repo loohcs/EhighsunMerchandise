a433ede9ad94cbf48d3cd00f3e602a475e06cd0c
loohcs <loohcs@126.com>
2014-01-22 17:52:00 +0800

    Initial Commit

---
+++ EhighsunMerchandise/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EhighsunMerchandise
+//
+//  Created by loohcs on 14-1-22.
+//  Copyright (c) 2014年 loohcs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
